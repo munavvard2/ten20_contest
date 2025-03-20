578fa35f379d30159c5b9494d4f44001a1f35f26
Munavvarhushen Durvesh <munavvard222@gmail.com>
2025-03-20 21:01:19 +0530

    homepage test failing / route, mix(older method) used instead of vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-        <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}">
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full text-sm mb-6 not-has-[nav]:hidden">
